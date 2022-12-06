3f74ac1944ad3e9c8ef88af4e9235a56dcf344b7
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-12-06 15:09:18 +0100

    Mise en page et finition du header

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const header = document.querySelector("header")
+const headerBtn = document.querySelector(".link__nav__btn")
 
 window.addEventListener("scroll", (e) => {
-    if (window.pageYOffset > 350) {
+    if (window.pageYOffset > 370) {
         header.style.backgroundColor = "white"
+        headerBtn.style.backgroundColor = "rgb(26, 137, 23)"
     }
-    else if (window.pageYOffset < 350) {
+    else if (window.pageYOffset < 370) {
         header.style.backgroundColor = "#F3C01B"
+        headerBtn.style.backgroundColor = "black"
     }
 })
